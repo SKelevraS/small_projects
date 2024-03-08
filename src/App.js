55427d49c6a1f3da6bacbd9d3d68f18f11b23977
shved.danila <shved.danila@wb.ru>
2024-03-08 18:38:53 +0300

    first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  function increment() {
+    setCount(count + 1)
+  }
+  function decrement() {
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счётчик</h2>
+        <h1>{count}</h1>
+        <button onClick={decrement} className='decrement'>Вычесть</button>
+        <button onClick={increment} className='increment'>Добавить</button>
+      </div>
+  
     </div>
   );
 }
