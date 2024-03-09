1d203b05fdada1b94d8f8f6d3075f7bfa395616f
shved.danila <shved.danila@wb.ru>
2024-03-09 14:30:02 +0300

    Добавлен Block, убраны функции с другой ветки

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-
-import { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-  function increment() {
-    setCount(count + 1)
-  }
-  function decrement() {
-    setCount(count - 1)
-  }
-
   return (
     <div className="App">
-  
+      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
   );
 }
