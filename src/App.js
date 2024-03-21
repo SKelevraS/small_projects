1ef9102bf30bb7ae3eaadbee567a0e1f2ceff505
shved.danila <shved.danila@wb.ru>
2024-03-21 19:45:50 +0300

    Добавлен основной функционал

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from 'react';
+import { Block } from './components/Block';
+import './index.css';
 
 function App() {
+  const [rates, setRates] = useState({})
+  const [fromCurrency, setFromCurrency] = useState('RUB')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(0)
+
+  useEffect(() => {
+    fetch('https://open.er-api.com/v6/latest/USD')
+    .then((res) => res.json())
+    .then((json) => {
+      setRates(json.rates)
+      console.log(json.rates)
+    })
+    .catch((err) => {
+      console.warn(err)
+      alert('Не получилось получить информацию')
+    })
+
+    
+  }, [])
+  const onChangeFromPrice = (value) =>{
+    const price = value / rates[fromCurrency]
+    const result = price * rates[toCurrency]
+    setToPrice(result)
+    setFromPrice(value)
+
+  }
+
+  const onChangeToPrice = (value) =>{
+
+    setFromPrice(value)
+
+  }
+  const onChangeFromCurrency = (cur) =>{
+    setFromCurrency(cur)
+    onChangeFromPrice(fromPrice)
+  }
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={onChangeFromCurrency} onChangeValue={onChangeFromPrice}  />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice}  />
     </div>
   );
 }
